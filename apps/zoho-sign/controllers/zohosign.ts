9869976fa1d2821da521acd4aa3caf55ac887363
navdeep-matrixecho <navdeepmatrixecho@gmail.com>
2024-09-12 16:04:45 +0530

    add seed

--- apps/zoho-sign/controllers/zohosign.ts
+++ apps/zoho-sign/controllers/zohosign.ts
@@ -1,36 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 const axios = require('axios');
 import { ZohoSignRepo } from '../models/zohosign';
+import { sendResponse } from "../../../libraries";
 
-const { ZOHO_CLIENT_ID, ZOHO_CLIENT_SECRET, ZOHO_REFRESH_TOKEN, ZOHO_API_BASE_URL } = process.env;
+const { ZOHO_API_BASE_URL } = process.env;
 
 export default class ZohoSignController {
-    static async oAuthCallback(req: Request, res: Response, next: NextFunction) {
-        const redirectUri = 'http://localhost:3001/api/zoho-sign/oauth/callback'; // Replace with your registered redirect URI
-
-        const authorizationCode = req.query.code;
-        console.log(authorizationCode)
-        try {
-            // const response = await axios.post('https://accounts.zoho.com/oauth/v2/token', null, {
-            //     params: {
-            //         grant_type: 'authorization_code',
-            //         client_id: ZOHO_CLIENT_ID,
-            //         client_secret: ZOHO_CLIENT_SECRET,
-            //         redirect_uri: redirectUri,
-            //         code: authorizationCode
-            //     }
-            // });
-            // console.log(response);
-
-            // const accessToken = response.data.access_token;
-            // console.log(accessToken);
-            // res.send('Authorization successful!', accessToken);
-
-        } catch (error) {
-            console.error('Error sending document for signing:', error);
-        }
-    }
-
     static async sendDocumentForSigning(req: Request, res: Response, next: NextFunction) {
         try {
             const documentDetails = {
@@ -51,14 +26,24 @@ export default class ZohoSignController {
                 ]
             };
 
-            const accessToken = '1000.fb032a7321fdf9a1a59575a9b373a21d.21fed82aa6f92c433d75890f9c99a518';
-            const response = await axios.post(`${ZOHO_API_BASE_URL}documents`, documentDetails, {
+            const accessToken = await new ZohoSignRepo().getAccessTokenZoho();
+            console.log(accessToken)
+            
+            const response = await axios.post('https://sign.zoho.com/api/v1/documents', documentDetails, {
                 headers: {
-                    'Authorization': `Zoho-oauthtoken ${accessToken}`,
-                    'Content-Type': 'application/json'
+                    'Authorization': `Bearer ${accessToken}`,
                 }
             });
+
             console.log('Document sent for signing:', response.data);
+            return res
+                .status(200)
+                .send(
+                    sendResponse(
+                        "",
+                        ""
+                    )
+                );
         } catch (error) {
             console.error('Error sending document for signing:', error);
         }
---
+++ src/config/config.ts
@@ -0,0 +1,51 @@
+import { Dialect } from 'sequelize';
+
+interface Config {
+  development: {
+    username: string;
+    password: string;
+    database: string;
+    host: string;
+    dialect: Dialect;
+  };
+  test: {
+    username: string;
+    password: string;
+    database: string;
+    host: string;
+    dialect: Dialect;
+  };
+  production: {
+    username: string;
+    password: string;
+    database: string;
+    host: string;
+    dialect: Dialect;
+  };
+}
+
+const config: Config = {
+  development: {
+    username: 'postgres',
+    password: 'root',
+    database: 'tongue_tingler',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  test: {
+    username: 'postgres',
+    password: 'root',
+    database: 'tongue_tingler',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  production: {
+    username: 'postgres',
+    password: 'root',
+    database: 'tongue_tingler',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+};
+
+export default config;

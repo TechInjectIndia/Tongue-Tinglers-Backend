1e0675660e988fd6700bac7564f697781839c98d
nitesh-techinject <nitesh.kumar@techinject.co.in>
2024-10-17 18:03:54 +0530

    post webhook instead of get

--- apps/payments/api/index.ts
+++ apps/payments/api/index.ts
@@ -227,7 +227,7 @@ const {
 // ====== Payments Starts ======
 router.post("/generate-link", hasPermission('payment', 'create'), validateGenerateLinkBody, PaymentsController.generatePaymentLink);
 router.get("/fetch-payment/:paymentId", hasPermission('payment', 'view'), PaymentsController.fetchPayment);
-router.get("/callback", PaymentsController.callback);
+router.post("/callback", PaymentsController.callback);
 // ====== Payments Ends ======
 
 export default router;

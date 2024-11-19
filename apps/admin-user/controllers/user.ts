56605c1c5969b2bda6f6e626c76affe315c7d167
nitesh-techinject <nitesh.kumar@techinject.co.in>
2024-11-19 15:13:19 +0530

    add prospect user

--- apps/admin-user/controllers/user.ts
+++ apps/admin-user/controllers/user.ts
@@ -1,11 +1,23 @@
 import { NextFunction, Request, Response } from "express";
 import { get, isEmpty } from "lodash";
-import { sendResponse, createPassword, createFirebaseUser, sendEmail, EMAIL_HEADING, getEmailTemplate, EMAIL_TEMPLATE } from "../../../libraries";
-import { RESPONSE_TYPE, SUCCESS_MESSAGE, ERROR_MESSAGE } from "../../../constants";
-import { AdminRepo } from '../models/user';
-import { Auth } from '../../auth/models';
-import { USER_TYPE } from '../../../interfaces';
-import { CONFIG } from '../../../config';
+import {
+    sendResponse,
+    createPassword,
+    createFirebaseUser,
+    sendEmail,
+    EMAIL_HEADING,
+    getEmailTemplate,
+    EMAIL_TEMPLATE,
+} from "../../../libraries";
+import {
+    RESPONSE_TYPE,
+    SUCCESS_MESSAGE,
+    ERROR_MESSAGE,
+} from "../../../constants";
+import { AdminRepo } from "../models/user";
+import { Auth } from "../../auth/models";
+import { USER_TYPE } from "../../../interfaces";
+import { CONFIG } from "../../../config";
 
 export default class AdminController {
     static async getAllUsers(req: Request, res: Response, next: NextFunction) {
@@ -22,7 +34,7 @@ export default class AdminController {
                 limit: size as number,
                 search: search as string,
                 sorting: sorting,
-                trashOnly: trashOnly as string
+                trashOnly: trashOnly as string,
             });
 
             return res
@@ -56,7 +68,7 @@ export default class AdminController {
                 limit: size as number,
                 search: search as string,
                 sorting: sorting,
-                trashOnly: trashOnly as string
+                trashOnly: trashOnly as string,
             });
 
             return res
@@ -78,10 +90,12 @@ export default class AdminController {
 
     static async addAdmin(req: Request, res: Response, next: NextFunction) {
         try {
-            const user_id = get(req, 'user_id', "");
+            const user_id = get(req, "user_id", "");
             const payload = { ...req?.body, createdBy: user_id };
 
-            const existingAdmin = await new Auth().getUserByEmail(payload.email);
+            const existingAdmin = await new Auth().getUserByEmail(
+                payload.email
+            );
             if (existingAdmin) {
                 return res
                     .status(400)
@@ -98,18 +112,13 @@ export default class AdminController {
                 emailVerified: true,
                 phoneNumber: payload.phoneNumber,
                 password: payload.password,
-                disabled: false
+                disabled: false,
             });
 
             if (!firebaseUser?.success) {
                 return res
                     .status(400)
-                    .send(
-                        sendResponse(
-                            RESPONSE_TYPE.ERROR,
-                            firebaseUser?.uid
-                        )
-                    );
+                    .send(sendResponse(RESPONSE_TYPE.ERROR, firebaseUser?.uid));
             }
 
             const hashedPassword = await createPassword(payload.password);
@@ -117,14 +126,17 @@ export default class AdminController {
                 ...payload,
                 password: hashedPassword,
                 type: USER_TYPE.MASTER_FRANCHISE,
-                firebaseUid: firebaseUser.uid
+                firebaseUid: firebaseUser.uid,
             });
 
             try {
-                const emailContent = await getEmailTemplate(EMAIL_TEMPLATE.WELCOME_ADMIN_USER, {
-                    email: payload.email,
-                    link: 'some-link',
-                });
+                const emailContent = await getEmailTemplate(
+                    EMAIL_TEMPLATE.WELCOME_ADMIN_USER,
+                    {
+                        email: payload.email,
+                        link: "some-link",
+                    }
+                );
 
                 const mailOptions = {
                     to: payload.email,
@@ -135,7 +147,11 @@ export default class AdminController {
                     },
                 };
 
-                await sendEmail(mailOptions.to, mailOptions.subject, mailOptions.templateParams);
+                await sendEmail(
+                    mailOptions.to,
+                    mailOptions.subject,
+                    mailOptions.templateParams
+                );
             } catch (emailError) {
                 console.error("Error sending email:", emailError);
             }
@@ -156,10 +172,71 @@ export default class AdminController {
         }
     }
 
+    static async addProspectUser(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) {
+        try {
+            const user_id = get(req, "user_id", "");
+            const payload = { ...req?.body, createdBy: user_id };
+
+            const existingAdmin = await new Auth().getUserByEmail(
+                payload.email
+            );
+            if (existingAdmin) {
+                return res
+                    .status(400)
+                    .send(
+                        sendResponse(
+                            RESPONSE_TYPE.ERROR,
+                            ERROR_MESSAGE.ADMIN_EXISTS
+                        )
+                    );
+            }
+
+            const firebaseUser = await createFirebaseUser({
+                email: payload.email,
+                emailVerified: true,
+                phoneNumber: payload.phoneNumber,
+                password: payload.password,
+                disabled: false,
+            });
+
+            if (!firebaseUser?.success) {
+                return res
+                    .status(400)
+                    .send(sendResponse(RESPONSE_TYPE.ERROR, firebaseUser?.uid));
+            }
+
+            const hashedPassword = await createPassword(payload.password);
+            await new AdminRepo().create({
+                ...payload,
+                password: hashedPassword,
+                type: USER_TYPE.PROSPECT,
+                firebaseUid: firebaseUser.uid,
+            });
+
+            return res
+                .status(200)
+                .send(
+                    sendResponse(
+                        RESPONSE_TYPE.SUCCESS,
+                        SUCCESS_MESSAGE.ADMIN_CREATED
+                    )
+                );
+        } catch (err) {
+            console.error("Error:", err);
+            return res.status(500).send({
+                message: err.message || ERROR_MESSAGE.INTERNAL_SERVER_ERROR,
+            });
+        }
+    }
+
     static async editAdmin(req: Request, res: Response, next: NextFunction) {
         try {
             const id = get(req?.params, "id", 0);
-            const user_id = get(req, 'user_id', 0);
+            const user_id = get(req, "user_id", 0);
             let payload = { ...req?.body, updatedBy: user_id };
 
             // if (payload.password) {
@@ -177,7 +254,7 @@ export default class AdminController {
                     )
                 );
         } catch (err) {
-            console.log(err)
+            console.log(err);
             return res.status(500).send({
                 message: ERROR_MESSAGE.INTERNAL_SERVER_ERROR,
             });
@@ -186,7 +263,7 @@ export default class AdminController {
 
     static async deleteAdmin(req: Request, res: Response, next: NextFunction) {
         try {
-            const user_id = get(req, 'user_id', 0);
+            const user_id = get(req, "user_id", 0);
             const ids = get(req?.body, "ids", "");
 
             await new AdminRepo().delete(ids, user_id);
@@ -241,7 +318,7 @@ export default class AdminController {
 
     static async editProfile(req: Request, res: Response, next: NextFunction) {
         try {
-            const id = get(req, "user_id",);
+            const id = get(req, "user_id");
             const payload = req?.body;
 
             await new AdminRepo().updateProfile(id, payload);

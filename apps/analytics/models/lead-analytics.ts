490591bf4c3b67527d2cbe4c699c8d9f5e484a49
Sumeet <55683378+TechInjectIndia@users.noreply.github.com>
2024-11-20 22:57:19 +0530

    fixing associations

--- apps/analytics/models/lead-analytics.ts
+++ apps/analytics/models/lead-analytics.ts
@@ -149,7 +149,7 @@ export class AnalyticsModel {
     public async leadTimelineForMasterFranchisee(startDate: Date, endDate: Date, groupBy: any, franchiseId: string): Promise<any> {
         if (!(startDate instanceof Date) || !(endDate instanceof Date) || startDate > endDate) {
             throw new Error('Invalid date range provided.');
-        } const franchiseRepo = new FranchiseeRepo();
+        }
 
         // Use PostgreSQL's date functions to format dates for grouping
         const dateAttribute = groupBy === "day"
@@ -209,8 +209,6 @@ export class AnalyticsModel {
         startDate: Date,
         endDate: Date,
         groupBy: any,
-        franchiseData: any,
-        franchiseId: string
     ): Promise<any> {
         if (!(startDate instanceof Date) || !(endDate instanceof Date) || startDate > endDate) {
             throw new Error('Invalid date range provided.');
@@ -226,48 +224,6 @@ export class AnalyticsModel {
                 [Op.between]: [startDate.toISOString(), endDate.toISOString()]
             }
         };
-
-        let campaignIds: string[] = [];
-        console.log('franchiseId.franchiseId', franchiseId);
-
-        if (franchiseId) {
-            const franchiseRepo = new FranchiseeRepo();
-            const franchiseData = await franchiseRepo.getFranchiseeById(franchiseId);
-
-            if (!franchiseData) {
-                throw new Error('Franchise data not found.');
-            }
-
-            switch (franchiseData.franchiseType) {
-                case FranchiseType.FRANCHISE:
-                    const campaignDataFranchise = await new CampaignAdRepo().getCampaignsByFranchiseId(franchiseId);
-                    campaignIds = campaignDataFranchise.map(campaign => campaign.id);
-                    break;
-                default:
-                    throw new Error('Invalid franchise type.');
-            }
-
-            if (campaignIds.length === 0) {
-                console.log('No campaigns found for this franchise.');
-                return []; // Return an empty array if no campaigns are found
-            }
-
-            // Add campaign ID filtering to where options
-            whereOptions.campaignId = { [Op.in]: campaignIds };
-        } else {
-            // Get all campaigns where franchiseData.id
-            console.log('id.id', franchiseData.id);
-
-            const campaignData = await new CampaignAdRepo().getCampaignsByFranchiseId(franchiseData.id);
-            console.log('campaignData:', campaignData);
-            campaignIds = campaignData.map(campaign => campaign.id);
-            console.log('campaignIds:', campaignIds);
-
-            if (campaignIds.length === 0) {
-                console.log('No campaigns found for the provided franchiseData.');
-                return []; // Return an empty array if no campaigns are found
-            }
-        }
 
         // Fetch leads where campaign IDs are present
         const data = await LeadsModel.findAll({

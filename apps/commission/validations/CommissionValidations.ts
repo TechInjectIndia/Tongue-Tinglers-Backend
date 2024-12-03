6798e7f727a716180b39173b3b79aba489404a41
Mandeep Singh <mandeep.techinject@gmail.com>
2024-12-03 09:56:14 +0530

    some changes

--- apps/commission/validations/CommissionValidations.ts
+++ apps/commission/validations/CommissionValidations.ts
@@ -63,7 +63,7 @@ const updateCommissionSchema = Joi.object({
     }),
 
     eventType: Joi.string()
-        .valid('franchise-sold', 'raw-material-sold')
+        .valid('master-franchise-franchise-sold', 'master-franchise-raw-material-sold', 'affiliate-franchise-sold', 'affiliate-raw-material-sold')
         .required()
         .messages({
             'any.required': 'Commission event type is required.',

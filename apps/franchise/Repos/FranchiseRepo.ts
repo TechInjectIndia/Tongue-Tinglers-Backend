7f0d8717f0ca58b315871237b872dc1fd8caba8c
nitesh-techinject <nitesh.techinject@gmail.com>
2024-12-03 16:51:22 +0530

    fix

--- apps/franchise/Repos/FranchiseRepo.ts
+++ apps/franchise/Repos/FranchiseRepo.ts
@@ -66,16 +66,24 @@ export class FranchiseRepo implements IFranchiseRepo {
     }
 
 
-    exists(email: string): Promise<boolean> {
-        return Promise.resolve(false);
+    async exists(email: string): Promise<boolean> {
+        try {
+            const res = await FranchiseModel.findOne({
+                where: { pocEmail: email },
+            });
+            return !!res;
+        } catch (e) {
+            console.log(e);
+            return false;
+        }
+
     }
 
     async getById(id: number): Promise<Franchise> {
         try {
-            const id2 = Number(id);
-            console.log("id", typeof id2);
+
             const res = (await FranchiseModel.findOne({
-                where: { id: Number(id2) },
+                where: { id: id },
                 include: [
                     {
                         model: RegionModel,
@@ -98,7 +106,10 @@ export class FranchiseRepo implements IFranchiseRepo {
                         as: "deletedByUser", // For the 'deletedBy' user
                     },
                 ],
-            })).toJSON();
+            }));
+            if (res) {
+                return res.toJSON();
+            }
             return res;
         } catch (e) {
             console.log(e);
@@ -106,8 +117,42 @@ export class FranchiseRepo implements IFranchiseRepo {
         }
     }
 
-    getByOrganizationId(organizationId: number): Promise<Franchise[]> {
-        return Promise.resolve([]);
+    async getByOrganizationId(organizationId: number): Promise<Franchise[]> {
+        try {
+            const res = await FranchiseModel.findAll({
+                where: { organizationId: organizationId },
+                include: [
+                    {
+                        model: RegionModel,
+                        as: "Region", // Matches the alias defined in the association
+                    },
+                    {
+                        model: OrganizationModel,
+                        as: "organization", // Matches the alias defined in the association
+                    },
+                    {
+                        model: UserModel,
+                        as: "createdByUser", // For the 'createdBy' user
+                    },
+                    {
+                        model: UserModel,
+                        as: "updatedByUser", // For the 'updatedBy' user
+                    },
+                    {
+                        model: UserModel,
+                        as: "deletedByUser", // For the 'deletedBy' user
+                    },
+                ],
+            });
+            if (res) {
+                return res.map((franchise) => franchise.toJSON());
+            } else {
+                return [];
+            }
+        } catch (e) {
+            console.log(e);
+            return [];
+        }
     }
 
     getByRegionId(regionId: number): Promise<Franchise[]> {

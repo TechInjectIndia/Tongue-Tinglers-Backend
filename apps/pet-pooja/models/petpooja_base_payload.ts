9751ddd7b1f84363a7c520b9e6fefea229de6a0b
Mandeep Singh <mandeep.techinject@gmail.com>
2024-12-06 13:58:53 +0530

    added PetPoojaSaveOrderPayload Model

---
+++ apps/pet-pooja/models/petpooja_base_payload.ts
@@ -0,0 +1,10 @@
+export interface PetPoojaBasePayload {
+    // Application key for authentication
+    app_key: string;
+
+    // Application secret for authentication
+    app_secret: string;
+
+    // Access token for authorization
+    access_token: string;
+}

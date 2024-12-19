6fc3bb010cfaca5ef6eecc57af873b395b80aafc
nitesh-techinject <nitesh.techinject@gmail.com>
2024-12-19 17:28:46 +0530

    fix

--- apps/cart-products/api/cartProductApi.ts
+++ apps/cart-products/api/cartProductApi.ts
@@ -1,16 +1,30 @@
 import * as express from 'express';
 import CartProductController from '../controllers/cartProductController';
-import { validateCreateCartProduct, validateDeleteCartProduct, validateUpdateQuantity } from "../validations/cartProductValidation";
-const {createCartProduct, updateQuantity, deleteCartProduct}  = CartProductController;
+import {
+    validateCreateCartProduct,
+    validateDeleteCartProduct,
+    validateUpdateQuantity
+} from "../validations/cartProductValidation";
+import CartDetailController
+    from "../../cart-details/controllers/cartDetailController";
+
+const {
+    createCartProduct,
+    updateQuantity,
+    deleteCartProduct
+} = CartProductController;
+const {getCartDetailByUserId} = CartDetailController;
 
 const router = express.Router();
 
+
+router.get('/detail', getCartDetailByUserId);
 router.post('/create', validateCreateCartProduct, createCartProduct);
 // router.get('/list',validateProductList ,getAllProducts);
 // router.get('/:id', validateProductById, getProductById);
 // router.put('/update/:id',validateProductById, validateUpdateProduct, updateProduct);
-router.put('/update-quantity/:id', validateUpdateQuantity ,updateQuantity);
-router.delete('/delete/:id', validateDeleteCartProduct ,deleteCartProduct);
+router.put('/update-quantity/:id', validateUpdateQuantity, updateQuantity);
+router.delete('/delete/:id', validateDeleteCartProduct, deleteCartProduct);
 // router.put('/update-status/:id',validateChangeProductStatus ,updateStatus);
 
 export default router;

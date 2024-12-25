9c218e1525f251eae2a120c27c89e29235d0ab88
rajinderkumar-techinject <rajinder.techinject@gmail.com>
2024-12-25 16:16:49 +0530

    completed presale flow

--- apps/checkout/controllers/CheckoutController.ts
+++ apps/checkout/controllers/CheckoutController.ts
@@ -2,7 +2,8 @@ import { Request, Response } from "express";
 import { get } from "lodash";
 import { ParsedOrder, PresaleParsedOrder } from "../../../interfaces";
 import { Cart } from "../../../interfaces/cart_products";
-
+import RepoProvider from "../../RepoProvider";
+import { DTO } from "../../common/models/DTO";
 
 interface OrderParams {
     userId: string;
@@ -12,21 +13,13 @@ interface OrderParams {
 }
 
 export default class CheckoutController {
-
-
     static getOrder(req: Request, res: Response): Promise<ParsedOrder> {
-
         const user_id = get(req, "user_id");
         //TODO @sumeet sir implement this
     }
 
-    static async getPreSaleOrder(req: Request, res: Response): Promise<PresaleParsedOrder> {
-        let cart = get(req, "cart");
-        if (!cart) {
-            return Promise.reject("Cart is empty");
-        }
+    static async getPreSaleOrder(req: Request, res: Response): Promise<DTO<PresaleParsedOrder>> {
+        return RepoProvider.getPreSaleOrderProvider.getPreSaleOrder(get(req, "cart"));
 
-
-        //TODO @sumeet sir implement this
     }
 }

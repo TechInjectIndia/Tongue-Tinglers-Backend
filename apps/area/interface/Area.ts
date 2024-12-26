a5e8123c5c0c3e80e7eac303c281346e09261f77
nitesh-techinject <nitesh.techinject@gmail.com>
2024-12-26 17:04:47 +0530

    fix

--- apps/area/interface/Area.ts
+++ apps/area/interface/Area.ts
@@ -1,3 +1,4 @@
+import { BaseMeta } from "@apps/common/models/ApiResponse";
 
 
 
--- database/schema/base/Base.ts
+++ database/schema/base/Base.ts
@@ -1,25 +1,25 @@
-import { MetaUser, ParsedUser } from "../../../interfaces";
+import { MetaUser } from "../../../interfaces/user";
 
-interface BaseMetaUsers {
-    createdBy: number;
-    updatedBy: number | null;
-    deletedBy: number | null;
-}
+// interface BaseMetaUsers {
+//     createdBy: number;
+//     updatedBy: number | null;
+//     deletedBy: number | null;
+// }
 
-interface BaseMeta extends BaseMetaUsers {
-    id: number;
-    createdAt: Date;
-    updatedAt: Date | null;
-    deletedAt: Date | null;
-}
+// interface BaseMeta extends BaseMetaUsers {
+//     id: number;
+//     createdAt: Date;
+//     updatedAt: Date | null;
+//     deletedAt: Date | null;
+// }
 
-interface ParsedMeta {
-    createdBy: MetaUser;
-    updatedBy: MetaUser | null;
-    deletedBy: MetaUser | null;
-    createdAt: Date;
-    updatedAt: Date | null;
-    deletedAt: Date | null;
-}
+// interface ParsedMeta {
+//     createdBy: MetaUser;
+//     updatedBy: MetaUser | null;
+//     deletedBy: MetaUser | null;
+//     createdAt: Date;
+//     updatedAt: Date | null;
+//     deletedAt: Date | null;
+// }
 
-export { BaseMeta, BaseMetaUsers,ParsedMeta };
+// export { BaseMeta, BaseMetaUsers,ParsedMeta };

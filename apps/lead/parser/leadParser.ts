ce8c447793f6b3ebf42556209c5582a6da8d391b
harshdalal-techinject <harshdalal.techinject@gmail.com>
2025-01-01 12:12:11 +0530

    remove logs from lead parser

--- apps/lead/parser/leadParser.ts
+++ apps/lead/parser/leadParser.ts
@@ -22,7 +22,6 @@ const parseLead = (lead: any): ParsedLead => {
         address: lead.address,
         additionalInfo: lead.additionalInfo,
         amount: lead.amount,
-        logs: lead.logs,
         followDetails: lead.followDetails
             ? lead.followDetails.map((detail: ParsedFollowDetails) =>
                   parseFollowDetails(detail)

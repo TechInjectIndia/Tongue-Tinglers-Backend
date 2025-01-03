e998978d1480899753c51de12e99e8d0c027600d
nitesh-techinject <nitesh.techinject@gmail.com>
2025-01-03 20:01:33 +0530

    merge nitesh-fix

--- config/database.ts
+++ config/database.ts
@@ -1,15 +1,15 @@
 import { Sequelize } from "sequelize";
 import { CONFIG } from "./environment";
 
-export const sequelize = new Sequelize(
-  CONFIG.DB_NAME,
-  CONFIG.DB_USERNAME,
-  CONFIG.DB_PASSWORD,
-  {
-    host: CONFIG.DB_HOST,
-    dialect: "postgres",
-    logging: false,
-  }
+// export const sequelize = new Sequelize(
+//   CONFIG.DB_NAME,
+//   CONFIG.DB_USERNAME,
+//   CONFIG.DB_PASSWORD,
+//   {
+//     host: CONFIG.DB_HOST,
+//     dialect: "postgres",
+//     logging: false,
+//   }
 
 export const sequelize  = new Sequelize(
     'postgresql://tt_owner:MEF2Rk7AhPdj@ep-lingering-snowflake-a5popb2a.us-east-2.aws.neon.tech/a',

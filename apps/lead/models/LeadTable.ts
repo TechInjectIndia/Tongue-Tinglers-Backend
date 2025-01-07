4b45abeef7f1c6da9946deebaecca4ca1cd5a735
sunil <sunilverma.techinject@gmail.com>
2025-01-07 12:04:42 +0530

    Added logs on Leads and follow details

--- apps/lead/models/LeadTable.ts
+++ apps/lead/models/LeadTable.ts
@@ -14,6 +14,7 @@ import {FollowDetails} from "apps/follow-details/interface/followDetails";
 import {UserModel} from "apps/user/models/UserTable";
 import {CampaignAdModel} from "apps/campaign/models/CampaignModel";
 import {sequelize} from "config";
+import { LogModel } from "apps/logs/models/LogsTable";
 
 const {STRING, TEXT, DATE, JSONB, ENUM, NOW} = DataTypes;
 
@@ -88,6 +89,12 @@ class LeadsModel extends Model<LeadTable, LeadCreationAttributes>
                                            // table
             as: "followDetails", // Alias for the relationship
         });
+        LeadsModel.hasMany(LogModel, {
+            foreignKey: "recordId", // The column in LogModel that references LeadsModel
+            sourceKey: "id",        // The primary key in LeadsModel
+            constraints: false,     // Disable constraints as `model` is dynamic
+            as: "logs",             // Alias for the association
+          });
     }
 
     public static initModel() {

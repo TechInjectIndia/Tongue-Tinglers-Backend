2df0df6974a7b574dd7ebecb138237b9e38361fe
nitesh-techinject <nitesh.techinject@gmail.com>
2025-01-15 11:19:54 +0530

    fix

--- apps/payments/controllers/index.ts
+++ apps/payments/controllers/index.ts
@@ -103,6 +103,10 @@ export default class PaymentsController {
             }
             else if(body.payload && body.payload.order  && body.payload.order.entity &&
                 body.payload.order.entity.status === "paid"){
+
+                console.log("3456y7u8io")
+                console.log(body.payload)
+                console.log("456789o0p")
                 const rpResponse = await RPOrderTable.findOne({where:{id:body.payload.order.entity.id}});
                 if(rpResponse){
                     // @TODO @rajinder sir this is temporary

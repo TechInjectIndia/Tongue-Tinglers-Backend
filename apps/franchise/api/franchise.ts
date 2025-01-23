e65914a6cd1d5afb1878d03c2065c2da84e75e87
sunil <sunilverma.techinject@gmail.com>
2025-01-23 16:55:32 +0530

    Added api for add user into franchise

--- apps/franchise/api/franchise.ts
+++ apps/franchise/api/franchise.ts
@@ -5,11 +5,12 @@ import { validateFranchiseList } from '../validations/franchiseValidations'
 
 const router = express.Router();
 
-const { createFranchise, getById, getAll } = FranchiseController;
+const { createFranchise, getById, getAll, addUserToFranchise } = FranchiseController;
 
 
 router.post("/create", hasPermission("admin", "get"), createFranchise);
 router.get("/get/:id", hasPermission("admin", "get"), getById);
 router.get('/list', validateFranchiseList, getAll)
+router.put('/addUserToFranchise/:id', addUserToFranchise)
 
 export default router;
